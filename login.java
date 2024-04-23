832e45748a146327baaade377eb54c243c454c12
Hala6069 <Halaaltehmaz@gmail.com>
2024-04-23 23:27:59 +0300

    updated version of login

--- login.java
+++ login.java
@@ -1,24 +1,37 @@
 import java.util.Scanner;
 
 public class login {
-    
+
+    // variables to check the code because there isno actual database
+
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "password123";
-    
+
+    // Scanner to read user's input
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Welcome to the Login System!");
         
         boolean isLoggedIn = false;
-        
+
+        // condition to check username and passsword
+
         while (!isLoggedIn) {
+
+            // printing "enter username" and read
+
             System.out.print("Enter username: ");
             String enteredUsername = scanner.nextLine();
             
+            // printing "enter password" and read password
+
             System.out.print("Enter password: ");
             String enteredPassword = scanner.nextLine();
             
+            // if statement
+
             if (authenticate(enteredUsername, enteredPassword)) {
                 System.out.println("Login successful!");
                 isLoggedIn = true;
@@ -27,7 +40,7 @@ public class login {
             }
         }
         
-        // Here you can add the main menu or functionality after successful login
+        // close scanner
         
         scanner.close();
     }
