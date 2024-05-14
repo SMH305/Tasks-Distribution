335a62d9936b2a72ed3d436019bf0ec33fe02c46
MalakAlmari <malakalmari2@gmail.com>
2024-05-14 18:35:18 +0300

    The updated versions of the classes after the review

--- borrowing/Book.java
+++ borrowing/Book.java
@@ -1,39 +1,40 @@
 package borrowing;
-
 class Book {
     private String title;
     private String ISBN;
+    private String author;
     private boolean isAvailable;
     private String borrower;
-
-    public Book(String title, String ISBN) {
+    
+    // Add the author of the book with the book class " Added After Reviewed"
+    public Book(String title, String ISBN, String author) {
         this.title = title;
         this.ISBN = ISBN;
+        this.author = author;
         this.isAvailable = true;
         this.borrower = null;
     }
-
-    // Get and Set Methods
-    public String getTitle() {
+    public String getTitle(){
         return title;
     }
-
     public String getISBN() {
         return ISBN;
+    }
+    
+    // Add the get method of author attribute with the book class " Added After Reviewed"
+    public String getAuthor() {
+        return author;
     }
 
     public boolean isAvailable() {
         return isAvailable;
     }
-
     public void setAvailable(boolean available) {
         isAvailable = available;
     }
-
     public String getBorrower() {
         return borrower;
     }
-
     public void setBorrower(String borrower) {
         this.borrower = borrower;
     }
